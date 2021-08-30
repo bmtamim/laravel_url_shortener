4180e8359020753a8199c2ed51a7fdf1ae2d5c80
Tamim Ikbal <bmtamim28@gmail.com>
2021-08-30 23:32:32 +0600

    Fix links query on ShortUrlAction

--- app/Actions/ShortUrlAction.php
+++ app/Actions/ShortUrlAction.php
@@ -6,6 +6,7 @@ namespace App\Actions;
 
 use App\DTO\ShortUrlDTO;
 use App\Models\Link;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
@@ -13,7 +14,11 @@ class ShortUrlAction
 {
     public function create(ShortUrlDTO $DTO): array
     {
-        if ($DTO::$links->where('user_ip', $DTO->user_ip)->count() >= 20) {
+        $links = $DTO::$links->when(Auth::check(), function ($query) {
+            $query->where('user_id', Auth::id());
+        })->where('user_ip', $DTO->user_ip);
+
+        if ($links->count() >= 20) {
             throw ValidationException::withMessages([
                 'link' => "You have reached your free plan limit. <a href='#'>Please Upgrade</a>",
             ]);
